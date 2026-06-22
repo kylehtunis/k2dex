c661cb50ff4de8b31ab8b303e21a6e0bd732f37f
Kyle Tunis <kyle.h.tunis@gmail.com>
2026-06-23 00:21:07 +0200

    new announcement

--- web/src/announcement.ts
+++ web/src/announcement.ts
@@ -1,4 +1,4 @@
 // Set this to a non-empty string to show a site-wide announcement banner.
 // Clear it (set to "") to hide the banner. Dismissed state is keyed on the
 // message text, so changing the text will re-show the banner for all visitors.
-export const ANNOUNCEMENT = "Regulation M-B Models will be added as soon as possible!";
+export const ANNOUNCEMENT = "Regulation M-B Model is now available!";
