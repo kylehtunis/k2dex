8fdb37c1c521a1033a7f4fc65d0a540e8a272b4e
Kyle Tunis <kyle.h.tunis@gmail.com>
2026-07-13 13:09:04 +0200

    update announcement banner

--- web/src/announcement.ts
+++ web/src/announcement.ts
@@ -1,4 +1,4 @@
 // Set this to a non-empty string to show a site-wide announcement banner.
 // Clear it (set to "") to hide the banner. Dismissed state is keyed on the
 // message text, so changing the text will re-show the banner for all visitors.
-export const ANNOUNCEMENT = "Regulation M-B Model is now available!";
+export const ANNOUNCEMENT = "k2dex has been updated to version 2, with significant improvements to the model and interface! Read about the changes: <a href='/articles/k2dex-v2-update'>k2dex v2 Update</a>.";
